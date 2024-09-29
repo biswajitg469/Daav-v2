a4db799fd4624e7c9271b0571171b5322181ef37
biswajitg469 <biswajitg469@gmail.com>
2024-09-29 13:29:26 +0530

    Push

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,28 @@
+{{-- <x-app-layout>
+    <x-slot name="header">
+        <h2 class="fw-semibold fs-4 text-dark">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-5">
+        <div class="container">
+            <div class="bg-white shadow-sm rounded">
+                <div class="p-4 text-dark">
+                    {{ __("You're logged in!") }}
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout> --}}
+
+<x-app-layout>
+
+    <x-slot name="title">
+        <h1>Hello</h1>
+    </x-slot>
+    <x-slot name="content">
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex earum consequatur impedit in deleniti, voluptatibus pariatur fugiat optio nesciunt provident repellat omnis natus dolor reiciendis error aut neque necessitatibus iste.</p>
+    </x-slot>
+</x-app-layout>
+
---
+++ resources/views/order/order_create.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+
+    <x-slot name="title">
+        <h1>Order Create</h1>
+    </x-slot>
+    <x-slot name="content">
+       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex earum consequatur impedit in deleniti, voluptatibus pariatur fugiat optio nesciunt provident repellat omnis natus dolor reiciendis error aut neque necessitatibus iste.</p>
+    </x-slot>
+</x-app-layout>
+
